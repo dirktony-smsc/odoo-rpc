526516bee219af979b8157d637e846adf1155d92
Dirk Tony <tonydirk.smsconsulting@gmail.com>
2026-03-12 12:43:28 +0300

    Update main.rs

--- crates/odoo-18-quotations-to-19/src/main.rs
+++ crates/odoo-18-quotations-to-19/src/main.rs
@@ -2,21 +2,31 @@ use odoo_rpc::{
     ModelName, OdooJsonRPCClient,
     utils::{Domain, PaginationParam, deserialize_and_default_if_false},
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::env::var;
 use struct_field_names_as_array::FieldNamesAsSlice;
 use url::Url;
 
-#[derive(Debug, Deserialize, FieldNamesAsSlice)]
-struct Partner {
-    id: u64,
+// #[derive(Debug, Deserialize, FieldNamesAsSlice)]
+// struct Partner {
+//     id: u64,
+//     name: String,
+//     #[serde(default, deserialize_with = "deserialize_and_default_if_false")]
+//     email: Option<String>,
+// }
+
+// impl ModelName for Partner {
+//     const NAME: &'static str = "res.partner";
+// }
+
+#[derive(Debug, Serialize)]
+struct TodoTask {
+    color: u8,
     name: String,
-    #[serde(default, deserialize_with = "deserialize_and_default_if_false")]
-    email: Option<String>,
 }
 
-impl ModelName for Partner {
-    const NAME: &'static str = "res.partner";
+impl ModelName for TodoTask {
+    const NAME: &'static str = "project.task";
 }
 
 #[tokio::main]
@@ -34,50 +44,62 @@ async fn main() -> anyhow::Result<()> {
     .await?;
     log::info!("client got!");
 
-    println!("uid = {:#?}", client_18.get_uid());
-    println!("version = {:#?}", client_18.version().await?);
+    // println!("uid = {:#?}", client_18.get_uid());
+    // println!("version = {:#?}", client_18.version().await?);
 
-    {
-        let a = client_18
-            .search_read_with_auto_model_name_and_field_names::<Partner>(
-                vec![Domain::new("is_company", "=", true)],
-                PaginationParam {
-                    offset: 0.into(),
-                    limit: 10.into(),
-                },
-            )
-            .await?;
-        let count = client_18
-            .search_count(
-                "res.partner".into(),
-                vec![Domain::new("is_company", "=", true)],
-            )
-            .await?;
-        println!("count {count}");
-        println!("{:#?}", a);
-    }
-    {
-        let ids = client_18
-            .search(
-                Partner::NAME.into(),
-                vec![Domain::new("is_company", "=", true)],
-                PaginationParam {
-                    offset: 10.into(),
-                    limit: 20.into(),
-                },
-            )
-            .await?;
-        let a: Vec<Partner> = client_18
-            .read_with_auto_model_name_and_field_names(ids)
-            .await?;
-        println!("{:#?}", a);
-    }
-    println!(
-        "{:#?}",
-        client_18
-            .fields_get(Partner::NAME.into(), Default::default(), Default::default())
-            .await?
-    );
-
+    // {
+    //     let a = client_18
+    //         .search_read_with_auto_model_name_and_field_names::<Partner>(
+    //             vec![Domain::new("is_company", "=", true)],
+    //             PaginationParam {
+    //                 offset: 0.into(),
+    //                 limit: 10.into(),
+    //             },
+    //         )
+    //         .await?;
+    //     let count = client_18
+    //         .search_count(
+    //             "res.partner".into(),
+    //             vec![Domain::new("is_company", "=", true)],
+    //         )
+    //         .await?;
+    //     println!("count {count}");
+    //     println!("{:#?}", a);
+    // }
+    // {
+    //     let ids = client_18
+    //         .search(
+    //             Partner::NAME.into(),
+    //             vec![Domain::new("is_company", "=", true)],
+    //             PaginationParam {
+    //                 offset: 10.into(),
+    //                 limit: 20.into(),
+    //             },
+    //         )
+    //         .await?;
+    //     let a: Vec<Partner> = client_18
+    //         .read_with_auto_model_name_and_field_names(ids)
+    //         .await?;
+    //     println!("{:#?}", a);
+    // }
+    // println!(
+    //     "{:#?}",
+    //     client_18
+    //         .fields_get(Partner::NAME.into(), Default::default(), Default::default())
+    //         .await?
+    // );
+    let res = client_18
+        .create_with_auto_module_name(vec![
+            TodoTask {
+                color: 8,
+                name: "Hello from tony odoo-rpc-rs again".into(),
+            },
+            TodoTask {
+                color: 2,
+                name: "Just a seccond thing ....".into(),
+            },
+        ])
+        .await?;
+    println!("{:#?}", res);
     Ok(())
 }
