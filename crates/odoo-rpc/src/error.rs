2ba9abf62701d6c15e4b4d46854bb9836903bdeb
Dirk Tony <tonydirk.smsconsulting@gmail.com>
2026-03-12 15:22:42 +0300

    non_exhaustive

--- crates/odoo-rpc/src/error.rs
+++ crates/odoo-rpc/src/error.rs
@@ -1,5 +1,6 @@
 #[derive(Debug, thiserror::Error)]
 #[error(transparent)]
+#[non_exhaustive]
 pub enum Error {
     RPCClient(#[from] jsonrpsee::core::ClientError),
     #[error("The current client is not logged in")]
