9995620a3c646e885c1bc8145542714d41ddffae
Dirk Tony <tonydirk.smsconsulting@gmail.com>
2026-04-02 11:54:12 +0300

    Update main.rs

--- crates/odoo-18-quotations-to-19/src/main.rs
+++ crates/odoo-18-quotations-to-19/src/main.rs
@@ -4,6 +4,7 @@ use std::process;
 async fn main() {
     env_logger::init();
     if let Err(err) = odoo_18_quotations_to_19::run().await {
+        log::error!("{:#?}", err);
         eprintln!("{err}");
         eprintln!("{}", err.backtrace());
         process::exit(1);
